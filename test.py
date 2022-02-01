a0a657c995bfe216f7fe2f6cbcb01ef8254a4642
Domattee <domattee@yahoo.de>
2022-02-01 11:33:35 +0100

    simply test file

---
+++ test.py
@@ -0,0 +1,21 @@
+import gym
+import time
+import numpy as np
+import mimoEnv
+
+env = gym.make("MIMo-v0")
+
+max_steps = 100
+
+obs = env.reset()
+
+for step in range(max_steps):
+    #action = env.action_space.sample()
+    action = np.zeros(env.action_space.shape)
+    obs, reward, done, info = env.step(action)
+    env.render()
+    time.sleep(1)
+    if done:
+        env.reset()
+
+env.close()
