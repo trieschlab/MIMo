97fcbee40e1e1a1ac432bd67a27cff324be31338
franmlopez <franmlopezg@gmail.com>
2022-02-22 18:27:15 +0100

    Fixed target, random initial positions

--- mimoEnv/__init__.py
+++ mimoEnv/__init__.py
@@ -3,3 +3,7 @@ from gym.envs.registration import register
 register(id='MIMo-v0',
          entry_point='mimoEnv.envs:MIMoTestEnv',
          )
+
+register(id='MIMoReach-v0',
+         entry_point='mimoEnv.envs:MIMoReachEnv',
+         )
--- mimoEnv/envs/__init__.py
+++ mimoEnv/envs/__init__.py
@@ -1,1 +1,2 @@
 from mimoEnv.envs.mimo_test import MIMoTestEnv
+from mimoEnv.envs.mimo_reach_env import MIMoReachEnv
