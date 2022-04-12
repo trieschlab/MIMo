3b6f018d3fff2fb27db36ae9db8b6a785f5a35da
Domattee <domattee@yahoo.de>
2022-04-12 06:04:37 +0200

    benchmark and test scrips protected with __name__ == structure

--- mimoEnv/test.py
+++ mimoEnv/test.py
@@ -3,21 +3,27 @@ import time
 import numpy as np
 import mimoEnv
 
-env = gym.make("MIMo-v0", show_sensors=True, print_space_sizes=True)
 
-max_steps = 250
+def main():
+    env = gym.make("MIMo-v0", show_sensors=False, print_space_sizes=True)
 
-obs = env.reset()
+    max_steps = 5000
 
-start = time.time()
-for step in range(max_steps):
-    #action = env.action_space.sample()
-    action = np.zeros(env.action_space.shape)
-    obs, reward, done, info = env.step(action)
-    #env.render()
-    #time.sleep(1)
-    if done:
-        env.reset()
+    _ = env.reset()
 
-print("Elapsed time: ", time.time() - start, "Simulation time:", max_steps*env.dt)
-env.close()
+    start = time.time()
+    for step in range(max_steps):
+        #action = env.action_space.sample()
+        action = np.zeros(env.action_space.shape)
+        obs, reward, done, info = env.step(action)
+        env.render()
+        #time.sleep(1)
+        if done:
+            env.reset()
+
+    print("Elapsed time: ", time.time() - start, "Simulation time:", max_steps*env.dt)
+    env.close()
+
+
+if __name__ == "__main__":
+    main()
