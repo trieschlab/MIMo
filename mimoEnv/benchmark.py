5938b0cb9c5819e80670fb8e184a7c8ccd3c0a0f
Domattee <domattee@yahoo.de>
2022-04-22 12:13:02 +0200

    API docs done

--- mimoEnv/benchmark.py
+++ mimoEnv/benchmark.py
@@ -1,3 +1,6 @@
+""" This module contains some functions to benchmark the performance of the simulation.
+"""
+
 import gym
 import time
 import copy
@@ -7,6 +10,12 @@ from mimoEnv.envs.mimo_env import DEFAULT_TOUCH_PARAMS
 
 
 def run(env, max_steps):
+    """ Runs an environment for a number of steps while taking random actions.
+
+    Args:
+        env (gym.Env): The environment.
+        max_steps (int): The number of time steps that we run the environment for.
+    """
     for step in range(max_steps):
         action = env.action_space.sample()
         obs, reward, done, info = env.step(action)
@@ -15,6 +24,17 @@ def run(env, max_steps):
 
 
 def benchmark():
+    """ Benchmarks multiple configurations for the sensor modules.
+
+    We use cProfile as the profiler. Multiple runs with different configurations are performed and their profiles saved
+    to a directory. Simulation time for one configuration is 1 hour with 60 episodes of one minute each.
+
+    Since vision and touch take up the majority of the runtime we focus on those. We test vision with resolutions of
+    64, 128, 256, and 512. We test touch by altering the sensor resolution with scale factors of 0.25, 0.5, 1.0 and 2.0.
+    Every combination of resolution and sensor scale is tested.
+
+    """
+
     # 1 hour simulation time, 1 minute episodes before reset. MIMO takes random actions.
     resolutions = [64, 128, 256, 512]
     scales = [0.25, 0.5, 1.0, 2.0]
