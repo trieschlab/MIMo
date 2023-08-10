3b29754d39c5004db545b30f4ee1c4e9b7294c7c
Domattee <domattee@yahoo.de>
2023-08-10 19:37:45 +0200

    Adjusted setup.py, hopefully installs in colab now?

--- setup.py
+++ setup.py
@@ -1,12 +1,21 @@
-from setuptools import setup
- 
+from setuptools import setup, find_packages
+import pkg_resources
+
+with open('requirements.txt') as requirements_txt:
+    install_requires = [
+        str(requirement)
+        for requirement
+        in pkg_resources.parse_requirements(requirements_txt)
+    ]
+
 setup(
     name='MIMo',
     version='1.1.0',
     url='',
     license='',
     author='Dominik Mattern, Francisco M. López, Pierre Schumacher',
-    packages=['mimoEnv', 'mimoVision', 'mimoVestibular', 'mimoProprioception', 'mimoTouch', 'mimoActuation'],
+    packages=find_packages(),
+    install_requires=install_requires,
     author_email='domattee@yahoo.de',
     description='MIMo library'
 )
